3923a2d6d7cae65182a6805687c246845bdd5a79
YasminW <155895681+YesminW@users.noreply.github.com>
2024-07-18 19:48:36 +0300

    commit birthday

---
+++ Co-P WebAPI/Controllers/BirthdayController.cs
@@ -0,0 +1,32 @@
+﻿using CO_P_library.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Co_P_WebAPI.Controllers
+{
+    public class BirthdayController : Controller
+    {
+        CoPFinalProjectContext db = new CoPFinalProjectContext();
+
+        [HttpGet]
+        [Route("current-month-birthdays")]
+        public IActionResult GetCurrentMonthBirthdays()
+        {
+            var currentMonth = DateTime.Now.Month;
+            var currentYear = DateTime.Now.Year;
+
+            var birthdays = db.Children
+                .Where(child => child.ChildBirthDate.Month == currentMonth)
+                .OrderBy(child => child.ChildBirthDate.Day)
+                .Select(child => new
+                {
+                    FullName = $"{child.ChildFirstName} {child.ChildSurname}",
+                    PhotoName = child.ChildPhotoName,
+                    BirthDate = child.ChildBirthDate.ToString("yyyy-MM-dd")
+                })
+                .ToList();
+
+            return Ok(birthdays);
+        }
+    }
+}
--- Co-P WebAPI/Controllers/UserInKindergartenControllerController.cs
+++ Co-P WebAPI/Controllers/UserInKindergartenControllerController.cs
@@ -10,48 +10,7 @@ namespace Co_P_WebAPI.Controllers
     {
         CoPFinalProjectContext db = new CoPFinalProjectContext();
 
-        //[HttpGet]
-        //[Route("ShowStaffInKindergarten")]
-        //public dynamic GetStaffmember(KindergartenYearDTO obj)
-        //{
-        //    var Stuff = db.UserInKindergartens.Where(x => x.User.UserCode == 111 && x.CurrentAcademicYear == obj.CurrentAcademicYear && x.KindergartenNumber == obj.KindergartenNumber).Select(u => new
-        //    {
-        //        FullName = u.User.UserPrivetName + " " + u.User.UserSurname,
-        //        PhoneNumber = u.User.UserPhoneNumber
-        //    });
-        //    return Ok(Stuff);
-
-        //}
-
-        //[HttpGet]
-        //[Route("ShowPerentsInKindergarten")]
-        //public IActionResult GetParents(KindergartenYearDTO obj)
-        //{
-        //    var parentsWithChildren = db.UserInKindergartens
-        //        .Where(uik => uik.User.UserCode == 222 &&
-        //                       uik.CurrentAcademicYear == obj.CurrentAcademicYear &&
-        //                       uik.KindergartenNumber == obj.KindergartenNumber)
-        //        .SelectMany(uik => uik.User.Parent.ChildParent1Navigations
-        //            .Select(child => new
-        //            {
-        //                ParentName = uik.User.UserPrivetName + " " + uik.User.UserSurname,
-        //                ParentPhoneNumber = uik.User.UserPhoneNumber,
-        //                ChildName = child.ChildFirstName + " " + child.ChildSurname
-        //            }))
-        //        .Concat(db.UserInKindergartens
-        //            .Where(uik => uik.User.UserCode == 222 &&
-        //                           uik.CurrentAcademicYear == obj.CurrentAcademicYear &&
-        //                           uik.KindergartenNumber == obj.KindergartenNumber)
-        //            .SelectMany(uik => uik.User.Parent.ChildParent2Navigations
-        //                .Select(child => new
-        //                {
-        //                    ParentName = uik.User.UserPrivetName + " " + uik.User.UserSurname,
-        //                    ParentPhoneNumber = uik.User.UserPhoneNumber,
-        //                    ChildName = child.ChildFirstName + " " + child.ChildSurname
-        //                })));
-
-        //    return Ok(parentsWithChildren);
-        //}
+       
 
     }
 }
