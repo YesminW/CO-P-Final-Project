f005a00fc3026cc5439e8af47cc53248e19e5d7f
liatReuven <liatreuven08@gmail.com>
2024-08-18 13:23:37 +0300

    סידור תפריט למטה

--- Final_Project/src/Elements/EfooterP.jsx
+++ Final_Project/src/Elements/EfooterP.jsx
@@ -1,43 +1,50 @@
-import logo from '/Images/logo.png'
-import '../assets/StyleSheets/Footer.css'
-import { Link } from 'react-router-dom';
+import logo from "/Images/logo.png";
+import "../assets/StyleSheets/Footer.css";
+import { Link } from "react-router-dom";
 
+import { Notifications, People } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
+import EditCalendarOutlinedIcon from "@mui/icons-material/EditCalendarOutlined";
+import RestaurantMenuOutlinedIcon from "@mui/icons-material/RestaurantMenuOutlined";
+import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
 
-import { Notifications, People } from '@mui/icons-material';
-import { IconButton } from '@mui/material';
-import EditCalendarOutlinedIcon from '@mui/icons-material/EditCalendarOutlined';
-import RestaurantMenuOutlinedIcon from '@mui/icons-material/RestaurantMenuOutlined';
-import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+const EfooterP = (
+  <div className="footer-icons center-a">
+    <Link style={{ width: "10%" }} to="/MainStaffMember">
+      <img src={logo} alt="My Logo" className="logofooter" />
+    </Link>
+    <Link to="/TeamStaff">
+      <IconButton>
+        <People sx={{ fontSize: 30 }} className="footer-icon" />
+      </IconButton>
+    </Link>
+    <Link to="/CalendarStaff">
+      <IconButton>
+        <EditCalendarOutlinedIcon
+          sx={{ fontSize: 30 }}
+          className="footer-icon"
+        />
+      </IconButton>
+    </Link>
+    <Link to="/Meals">
+      <IconButton>
+        <RestaurantMenuOutlinedIcon
+          sx={{ fontSize: 30 }}
+          className="footer-icon"
+        />
+      </IconButton>
+    </Link>
+    <Link>
+      <IconButton>
+        <Notifications sx={{ fontSize: 30 }} className="footer-icon" />
+      </IconButton>
+    </Link>
+    <Link to="/ChatList">
+      <IconButton>
+        <SendOutlinedIcon sx={{ fontSize: 30 }} className="footer-icon" />
+      </IconButton>
+    </Link>
+  </div>
+);
 
-
-const Efooterp =
-    <div className="footer-icons">
-        <Link style={{width: '10%'}} to="/MainParent">
-            <img src={logo} alt="My Logo" className="logofooter" />
-        </Link>
-        <Link to="/MainParent">
-            <IconButton >
-                <People className="footer-icon" />
-            </IconButton>
-        </Link>
-        <Link>
-            <IconButton >
-                <EditCalendarOutlinedIcon className="footer-icon" />
-            </IconButton>
-        </Link>
-        <Link>
-            <IconButton >
-                <RestaurantMenuOutlinedIcon className="footer-icon" />
-            </IconButton>
-        </Link>
-        <Link>
-            <IconButton >
-                <Notifications className="footer-icon" />
-            </IconButton>
-        </Link>
-        <IconButton >
-            <SendOutlinedIcon className="footer-icon" />
-        </IconButton>
-    </div>
-
-export default Efooterp;
+export default EfooterP;
