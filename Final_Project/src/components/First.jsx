d84de649c74e3b918949c0a110b56f0b109bef8f
liatReuven <liatreuven08@gmail.com>
2024-08-18 14:11:18 +0300

    סידור מסך ראשי

--- Final_Project/src/components/First.jsx
+++ Final_Project/src/components/First.jsx
@@ -1,31 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 import Elogo from "../Elements/Elogo";
 
 import "../assets/StyleSheets/Main.css";
 
 export default function First() {
-  const apiUrl = "http://localhost:5108/getAllUsers";
-
   return (
     <div>
       {Elogo}
       <br />
       <h1 className="loginh1">מי אתה/את?</h1>
-      <div className="buttons">
-        <Link to="/LoginParent">
-          <button className="btn">הורה</button>
+      <div className="login-buttons">
+        <Link to="/LoginParent" className="btn">
+          הורה
         </Link>
-        <Link to="/LoginStaffMember">
-          <button className="btn">איש צוות</button>
-        </Link>
-      </div>
-      <div>
-        <Link to="/LoginManage">
-          <button className="btn">מנהל/ת חינוך</button>
+        <Link to="/LoginStaffMember" className="btn">
+          איש צוות
         </Link>
       </div>
+      <Link to="/LoginManage">
+        <button className="btn">מנהל/ת חינוך</button>
+      </Link>
     </div>
   );
 }
