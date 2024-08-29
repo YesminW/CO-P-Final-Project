e4023c0d8c7206498be3d0892f247993c7d171ad
YasminW <155895681+YesminW@users.noreply.github.com>
2024-08-29 20:48:54 +0300

    commit datetime

--- Co-P WebAPI/Controllers/ActivityController.cs
+++ Co-P WebAPI/Controllers/ActivityController.cs
@@ -1,4 +1,5 @@
 ﻿using CO_P_library.Models;
+using Co_P_WebAPI.DTO;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Co_P_WebAPI.Controllers
@@ -32,20 +33,47 @@ namespace Co_P_WebAPI.Controllers
 
         [HttpPost]
         [Route("createSummary")]
-        public dynamic createSummary (int CurrentAcademicYear, int kindernumber, string Daysummary)
+        public dynamic createSummary (int CurrentAcademicYear, int kindernumber, string Daysummary, DateTime today)
         {
             DaySummary newSummary = new DaySummary();
-            newSummary.DaySummaryDate = DateTime.Now;
-            newSummary.DaySummaryHour = DateTime.Hour;
+            newSummary.DaySummaryDate = today;
+            newSummary.DaySummaryHour = today.Hour;
             newSummary.SummaryDetails = Daysummary;
             newSummary.KindergartenNumber = kindernumber;
             newSummary.CurrentAcademicYear = CurrentAcademicYear;
 
             db.DaySummaries.Add(newSummary);
             db.SaveChanges();
-            return newSummary;
-            
+            return newSummary; 
 
+        }
+
+        [HttpGet]
+        [Route("getbydateandkindergarten/{kindergartenNumner}/{today}")]
+        public dynamic Getbydateandkindergarten(int kindergartenNumner, DateTime today)
+        {
+            var ActualActivities = db.ActualActivities.Where(a => a.ActivityDate == today && a.KindergartenNumber == kindergartenNumner && a.ActivityNumber == 10).Select(m => new MealsInKindergartenDTO()
+            {
+                ActivityDate = m.ActivityDate,
+                KindergartenName = m.KindergartenNumberNavigation.KindergartenName,
+                MaelName = m.MealNumberNavigation.MealType,
+                MealDetails = m.MealNumberNavigation.MealDetails
+
+
+            });
+            return ActualActivities;
+        }
+
+        [HttpPut]
+        [Route("Editbydateandkindergarten")]
+        public dynamic Editbydateandkindergarten(int kindergartenNumner, DateTime date, string mealName, string mealDetail)
+        {
+            var a = db.ActualActivities.Where(a => a.ActivityDate == date && a.KindergartenNumber == kindergartenNumner && a.MealNumberNavigation.MealType == mealName).FirstOrDefault();
+            a.MealNumberNavigation.MealDetails = mealDetail;
+
+            db.SaveChanges();
+
+            return ("UpDate ");
         }
 
 
